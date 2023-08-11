dab126b9aacac43793374009c7663eb60316b23e
Jorshstar <ajosh22.aj@gmail.com>
2023-08-11 04:51:49 +0000

    updated

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,41 @@
 #!/usr/bin/node
-/**
- * Prints all characters of a Star Wars movie
- * The first positional argument passed is the Movie ID
- * Display one character name per line in the same order
- * as  list in the /films/ endpoint
- */
-
 const request = require('request');
-const filmNum = process.argv[2] + '/';
-const filmURL = 'https://swapi-api.hbtn.io/api/films/';
-// Makes API request, sets async to allow await promise
-request(filmURL + filmNum, async (err, res, body) => {
-  if (err) return console.error(err);
 
-  // find URLs of each character in the film as a list obj
-  const charURLList = JSON.parse(body).characters;
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
 
-  // Use URL list to character pages to make new requests
-  // await queues requests until they resolve in order
-  for (const charURL of charURLList) {
-    await new Promise((resolve, reject) => {
-      request(charURL, (err, res, body) => {
-        if (err) return console.error(err);
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-        // finds each character name and prints in URL order
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
   }
+  
+  if (response.statusCode !== 200) {
+    console.error('Status code:', response.statusCode);
+    return;
+  }
+  
+  const movieData = JSON.parse(body);
+  
+  movieData.characters.forEach((characterUrl) => {
+    request(characterUrl, (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error('Error:', charError);
+        return;
+      }
+      
+      if (charResponse.statusCode !== 200) {
+        console.error('Status code:', charResponse.statusCode);
+        return;
+      }
+      
+      const characterData = JSON.parse(charBody);
+      console.log(characterData.name);
+    });
+  });
 });
